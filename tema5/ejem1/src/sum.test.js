7afcce59fced82a85768b5e933dd98241fefd061
micaelgallego <micael.gallego@gmail.com>
2019-12-18 02:48:38 +0100

    Fix minor issues in tema5 ejem1

--- tema5/ejem1/src/sum.test.js
+++ tema5/ejem1/src/sum.test.js
@@ -5,7 +5,7 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(4);
+    expect(sum(1, 2)).toBe(3);
 });
 
 test('adds 1 + 2 to equal 3', () => {
