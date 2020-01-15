c618984616065bb6f98ad512a6839cca6746094b
micaelgallego <micael.gallego@gmail.com>
2020-01-15 23:37:45 +0100

    Tema12 Ejer1 Enun

---
+++ tema12/ejer1-enun/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input v-model="input">
+    <button @click="addElem()">Añadir</button>
+    <ul>
+      <li v-for="elem in elems" :key="elem">{{elem}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data: () => {
+    return {
+      input: "",
+      elems: []
+    }
+  },
+  methods:{
+    addElem: function(){
+      this.elems.push(this.input);
+      this.input = "";
+    }
+  }
+};
+</script>
