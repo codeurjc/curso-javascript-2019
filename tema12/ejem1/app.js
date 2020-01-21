89bf550b273023ddb398e8ecd471eee55d0e5699
micaelgallego <micael.gallego@gmail.com>
2020-01-21 22:54:28 +0100

    Add sleeps to ejem1

--- tema12/ejem1/app.js
+++ tema12/ejem1/app.js
@@ -1,12 +1,22 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 
+async function sleep(){
+    return new Promise(resolve=> setTimeout(resolve, 2000))
+}
+
 async function browse() {
 
     let driver = await new Builder().forBrowser('chrome').build();
     try {
 
         await driver.get('http://www.google.com/ncr');
+
+        await sleep();
+
         await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+
+        await sleep();
+
         await driver.wait(until.titleContains('webdriver'), 5000);
         console.log("Google works as expected");
 
